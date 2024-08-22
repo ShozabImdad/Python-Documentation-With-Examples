1c064c1d88840879fe7b7a485fe42cb571f88dae
ShozabImdad <imdadshozab@gmail.com>
2024-08-22 16:36:03 +0500

    added modules file and module calc

---
+++ calc.py
@@ -0,0 +1,14 @@
+# Creating a simple module for practicing modules in python
+
+def add(x, y):
+    return(x+y)
+
+def subtract(x, y):
+    return (x-y)
+
+def divide(x, y):
+    return(x/y)
+
+
+def multiply(x, y):
+    return(x*y)
